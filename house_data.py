5352802920dc02e7b549f52d4a14f67af9fa0040
Laura Sun <sunbutterfly@mac.com>
2017-09-21 13:29:05 -0700

    Alameda

--- house_data.py
+++ house_data.py
@@ -8,13 +8,17 @@ xls_path = '/Users/laura/desktop/2017-08_Median_Prices_of_Existing_Detached_Home
 def data_for_region(xls_path, region):
     workbook = openpyxl.load_workbook(xls_path)
     sheet = workbook.get_sheet_by_name('Median Price')
-    for row in sheet.iter_rows(min_row=8, min_col=1, max_row=10, max_col=4):
+    for row in sheet.iter_rows(min_row=8,min_col=1, max_row=10, max_col=4):
 
-        for cell in row:
-            value = cell.value
-            print(value, end=' ')
-        print()
-
+        date = row[0].value
+        amount = row[2].value
+        print(date,amount)
 
 
-data_for_region(xls_path,'CA')
+
+
+
+
+
+
+data_for_region(xls_path,'Alameda')
