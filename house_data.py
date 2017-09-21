b06f7ef0f913afff3afac9e808bceb19b3ebdf66
Laura Sun <sunbutterfly@mac.com>
2017-09-21 13:05:32 -0700

    data for region function

--- house_data.py
+++ house_data.py
@@ -1,16 +1,20 @@
 
 
 import openpyxl
-
-xls_path ='/Users/laura/desktop'
-workbook = openpyxl.load_workbook('2017-08_Median_Prices_of_Existing_Detached_Homes_(Historical_Data).xlsx')
-sheet = workbook.get_sheet_by_name('Median Price')
+xls_path = '/Users/laura/desktop/2017-08_Median_Prices_of_Existing_Detached_Homes_(Historical_Data).xlsx'
 
 
-for row in sheet.iter_rows(min_row=8, min_col=1, max_row=10, max_col=4):
 
-    for cell in row:
-        value = cell.value
-        print(value, end=' ')
-    print()
+def data_for_region(xls_path, region):
+    workbook = openpyxl.load_workbook(xls_path)
+    sheet = workbook.get_sheet_by_name('Median Price')
+    for row in sheet.iter_rows(min_row=8, min_col=1, max_row=10, max_col=4):
 
+        for cell in row:
+            value = cell.value
+            print(value, end=' ')
+        print()
+
+
+
+data_for_region(xls_path,'CA')
