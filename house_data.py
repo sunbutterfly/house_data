f6cbc2eccb12182b4555db57e4defc8ced6cd627
Laura Sun <sunbutterfly@mac.com>
2017-09-21 19:35:35 -0700

    finding position

--- house_data.py
+++ house_data.py
@@ -3,22 +3,32 @@
 import openpyxl
 xls_path = '/Users/laura/desktop/2017-08_Median_Prices_of_Existing_Detached_Homes_(Historical_Data).xlsx'
 
+def get_row_values(worksheet, row_num):
+    rows = list(worksheet.iter_rows(min_row=row_num,max_row=row_num))
+    return[cell.value for cell in rows[0]]
+
+
 
 
 def data_for_region(xls_path, region):
     workbook = openpyxl.load_workbook(xls_path)
     sheet = workbook.get_sheet_by_name('Median Price')
-    for row in sheet.iter_rows(min_row=8,min_col=1, max_row=10, max_col=4):
+    headings = get_row_values(sheet, 8)
 
+    position = 0
+    for heading in headings:
+        if region == heading:
+            print(str(position))
+            break
+        position +=1
+
+    for row in sheet.iter_rows(min_row=9,min_col=1, max_row=10, max_col=4):
         date = row[0].value
-        amount = row[2].value
+        amount = row[position].value
         print(date,amount)
 
 
 
 
 
-
-
-
-data_for_region(xls_path,'Alameda')
+data_for_region(xls_path,'Amador')
